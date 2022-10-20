4bd5d800dcd94d7d513e86a1a73fa0a196037667
Rafael <rafael.leoanrdi98@gmail.com>
2022-10-20 15:57:33 +0200

    Tipo biglietto, carozza e codice CP

--- main.js
+++ main.js
@@ -24,29 +24,66 @@ function generaInfoBiglietto(){
     // Condizione sull'eta
     if(eta === "minorenne"){
 
+        let tipoOfferta = "Biglietto Ridotto";
+        document.getElementById("tipologiaBiglietto").innerHTML= tipoOfferta;
         // Valore prezzo viaggio se minorenne
         prezzoBiglietto= (km * kmPrice) - (km * kmPrice) * 0.2;
 
         // Stampa del prezzo viaggio
         document.getElementById("prezzoViaggio").innerHTML= `${prezzoBiglietto.toFixed(2)}`;
         console.log(prezzoBiglietto);
+
+        // Numeri Randomici Carozza
+        let carozza = Math.floor((Math.random()*10)+1);
+        document.getElementById("carozza").innerHTML= carozza;
+        // Numeri Randomici codice CP
+        let codiceCP = Math.floor(Math.random()*10000);
+        document.getElementById("codiceCP").innerHTML= codiceCP;
+        // stampa nome
+        document.getElementById("nomePasseggero").innerHTML= nome;
     }
     else if (eta === "over65"){
+
+        let tipoOfferta = "Biglietto Ridotto";
+        document.getElementById("tipologiaBiglietto").innerHTML= tipoOfferta;
+
         // Valore prezzo viaggio se over65
         prezzoBiglietto= (km * kmPrice) - (km * kmPrice) * 0.4;
 
         // Stampa del prezzo viaggio
         document.getElementById("prezzoViaggio").innerHTML= `${prezzoBiglietto.toFixed(2)}`;
         console.log(prezzoBiglietto);
+
+        // Numeri Randomici Carozza
+        let carozza = Math.floor((Math.random()*10)+1);
+        document.getElementById("carozza").innerHTML= carozza;
+        // Numeri Randomici codice CP
+        let codiceCP = Math.floor(Math.random()*10000);
+        document.getElementById("codiceCP").innerHTML= codiceCP;
+        // stampa nome
+        document.getElementById("nomePasseggero").innerHTML= nome;
     }
     else{
+
+        let tipoOfferta = "Biglietto Base";
+        document.getElementById("tipologiaBiglietto").innerHTML= tipoOfferta;
+
         // Valore prezzo viaggio se magiorenne
         prezzoBiglietto= km * kmPrice;
 
         // Stampa del prezzo viaggio
         document.getElementById("prezzoViaggio").innerHTML= `${prezzoBiglietto.toFixed(2)}`;
         console.log(prezzoBiglietto);
-        
+
+        // Numeri Randomici Carozza
+        let carozza = Math.floor((Math.random()*10)+1);
+        document.getElementById("carozza").innerHTML= carozza;
+        // Numeri Randomici codice CP
+        let codiceCP = Math.floor(Math.random()*10000);
+        document.getElementById("codiceCP").innerHTML= codiceCP;
+        // stampa nome
+        document.getElementById("nomePasseggero").innerHTML= nome;
+
     }
 }
 
