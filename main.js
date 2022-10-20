bc2a95a2e4a99eda60618ecc05e26fd571affbc9
Rafael <rafael.leoanrdi98@gmail.com>
2022-10-20 15:07:16 +0200

    Input nome e km

---
+++ main.js
@@ -0,0 +1,14 @@
+function generaInfoBiglietto(){
+
+    let kmPrice= 0.21;
+    console.log(kmPrice);
+
+    let prezzoBiglietto;
+
+    let nome = document.getElementById("inputNome").value;
+    console.log(nome);
+
+    let km = document.getElementById("inputKm").value; 
+    console.log(km);
+}
+
